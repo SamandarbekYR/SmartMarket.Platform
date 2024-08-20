e5a6a4b1751cc72c30ef1e7e1bb6a2defdef33f9
Ozodbek Umarov <ozodchik.krasavchik@gmail.com>
2024-08-20 16:22:50 +0500

    Added AddCategoryDto

---
+++ src/SmartMarket.Service/DTOs/Category/AddCategoryDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartMarket.Service.DTOs.Category;
+
+public class AddCategoryDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
