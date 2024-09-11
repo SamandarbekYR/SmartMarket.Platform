19068c73e895a788a13c1dbebefc83fffac0de08
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-09-11 15:34:29 +0500

    Updated UI

--- src/SmartMarket.Desktop/Pages/ShopDetailsForPage/ShopDetailsPage.xaml.cs
+++ src/SmartMarket.Desktop/Pages/ShopDetailsForPage/ShopDetailsPage.xaml.cs
@@ -1,18 +1,5 @@
-﻿using SmartMarket.Desktop.Components.ShopDetailsForComponent;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartMarket.Desktop.Pages.ShopDetailsForPage
 {
@@ -33,25 +20,75 @@ namespace SmartMarket.Desktop.Pages.ShopDetailsForPage
         {
             ShopHIstoryPage shopHIstoryPage = new ShopHIstoryPage();
             SalePageNavigator.Content = shopHIstoryPage;
+
+            Main_Border.Visibility = Visibility.Visible;      
+
+            Invalid_Prods_1.Visibility = Visibility.Collapsed;
+            Invalid_Prods_2.Visibility = Visibility.Collapsed;
+
+            Return_Prods_1.Visibility = Visibility.Collapsed;
+            Return_Prods_2.Visibility = Visibility.Collapsed;
+
+            History_Border_1.Visibility = Visibility.Visible;
+            History_Border_2.Visibility = Visibility.Visible;
+            History_Border_3.Visibility = Visibility.Visible;
+
         }
 
         private void rbTopSale_Click(object sender, RoutedEventArgs e)
         {
             TopSalePage topSalePage = new TopSalePage();    
             SalePageNavigator.Content= topSalePage;
-            
+
+            Main_Border.Visibility = Visibility.Collapsed;
+
+            History_Border_1.Visibility = Visibility.Collapsed;
+            History_Border_2.Visibility = Visibility.Collapsed;
+            History_Border_3.Visibility = Visibility.Collapsed;
+
+            Invalid_Prods_1.Visibility = Visibility.Collapsed;
+            Invalid_Prods_2.Visibility = Visibility.Collapsed;
+
+            Return_Prods_1.Visibility = Visibility.Collapsed;
+            Return_Prods_2.Visibility = Visibility.Collapsed;
+
         }
 
         private void rbReturnProds_Click(object sender, RoutedEventArgs e)
         {
-          ReturnedCargoPage returnedCargoPage = new ReturnedCargoPage();
-           SalePageNavigator.Content= returnedCargoPage;    
+            ReturnedCargoPage returnedCargoPage = new ReturnedCargoPage();
+            SalePageNavigator.Content= returnedCargoPage;
+
+            Main_Border.Visibility = Visibility.Visible;
+
+            History_Border_1.Visibility = Visibility.Collapsed;
+            History_Border_2.Visibility = Visibility.Collapsed;
+            History_Border_3.Visibility = Visibility.Collapsed;
+
+            Invalid_Prods_1.Visibility = Visibility.Collapsed;
+            Invalid_Prods_2.Visibility = Visibility.Collapsed;
+
+            Return_Prods_1.Visibility = Visibility.Visible;
+            Return_Prods_2.Visibility = Visibility.Visible;
+
         }
 
         private void rbInvalidProds_Click(object sender, RoutedEventArgs e)
         {
             InValidProductPage inValidProductPage = new InValidProductPage();
             SalePageNavigator.Content= inValidProductPage;
+
+            Main_Border.Visibility = Visibility.Visible;
+
+            History_Border_1.Visibility = Visibility.Collapsed;
+            History_Border_2.Visibility = Visibility.Collapsed;
+            History_Border_3.Visibility = Visibility.Collapsed;
+
+            Return_Prods_1.Visibility = Visibility.Collapsed;
+            Return_Prods_2.Visibility = Visibility.Collapsed;
+
+            Invalid_Prods_1.Visibility = Visibility.Visible;
+            Invalid_Prods_2.Visibility = Visibility.Visible;
         }
 
 
