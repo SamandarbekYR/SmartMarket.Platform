865108d16d65647e45317495433eb2ef4da21a2b
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-09-11 21:21:42 +0500

    UI updated

--- src/SmartMarket.Desktop/Pages/SettingsForPage/SettingsScalesPage.xaml.cs
+++ src/SmartMarket.Desktop/Pages/SettingsForPage/SettingsScalesPage.xaml.cs
@@ -1,49 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SmartMarket.Desktop.Components.SettingsForComponent;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using SmartMarket.Desktop.Components.SettingsForComponent;
 
-namespace SmartMarket.Desktop.Pages.SettingsForPage
+namespace SmartMarket.Desktop.Pages.SettingsForPage;
+
+/// <summary>
+/// Interaction logic for SettingsScalesPage.xaml
+/// </summary>
+public partial class SettingsScalesPage : Page
 {
-    /// <summary>
-    /// Interaction logic for SettingsScalesPage.xaml
-    /// </summary>
-    public partial class SettingsScalesPage : Page
+    public SettingsScalesPage()
     {
-        public SettingsScalesPage()
-        {
-            InitializeComponent();
-            GetScales();
-        }
+        InitializeComponent();
+        GetScales();
+    }
 
-        private void bntAddScales_Click(object sender, RoutedEventArgs e)
-        {
+    private void bntAddScales_Click(object sender, RoutedEventArgs e)
+    {
 
-        }
+    }
 
 
-        public void GetScales()
-        {
-            St_Scales.Visibility = Visibility.Visible;
-            St_Scales.Children.Clear();
+    public void GetScales()
+    {
+        St_Scales.Visibility = Visibility.Visible;
+        St_Scales.Children.Clear();
 
-            SettingsScalesComponent settingsScalesComponent = new SettingsScalesComponent();
-            settingsScalesComponent.Tag = 1;
-            settingsScalesComponent.SetData("Tarozi 1:");
-            settingsScalesComponent.BorderThickness = new Thickness(0, 0, 0, 5);
-            St_Scales.Children.Add(settingsScalesComponent);
+        SettingsScalesComponent settingsScalesComponent = new SettingsScalesComponent();
+        settingsScalesComponent.Tag = 1;
+        settingsScalesComponent.SetData("Tarozi 1:");
+        settingsScalesComponent.BorderThickness = new Thickness(0, 0, 0, 5);
+        St_Scales.Children.Add(settingsScalesComponent);
 
-        }
     }
 }
