d6419a5c7742d483611d3807b71cff1b8330effc
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-09-17 15:51:20 +0500

    push

--- src/SmartMarket.Desktop/Pages/SaleForPage/SalePage.xaml.cs
+++ src/SmartMarket.Desktop/Pages/SaleForPage/SalePage.xaml.cs
@@ -4,108 +4,112 @@ using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace SmartMarket.Desktop.Pages.SaleForPage
+namespace SmartMarket.Desktop.Pages.SaleForPage;
+
+/// <summary>
+/// Interaction logic for SalePage.xaml
+/// </summary>
+public partial class SalePage : Page
 {
-    /// <summary>
-    /// Interaction logic for SalePage.xaml
-    /// </summary>
-    public partial class SalePage : Page
+    private System.Timers.Timer timer = new System.Timers.Timer();
+
+
+    int activeTextboxIndex = 2;
+    string barcode = "";
+
+    public SalePage()
     {
-        private System.Timers.Timer timer = new System.Timers.Timer();
+        InitializeComponent();
+        GetData();
+        timer.Elapsed += vaqt_ketdi;
+        timer.Interval = 500;
+        timer.Enabled = true;
+    }
 
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
 
-        int activeTextboxIndex = 2;
-        string barcode = "";
+        var button = (Button)sender;
+        WriteNumber(button.Content.ToString() ?? "");
 
-        public SalePage()
+    }
+
+    private void btnClear_Click(object sender, RoutedEventArgs e)
+    {
+        tbCalculator.Text = string.Empty;
+    }
+
+    private void btnBackKlav_Click(object sender, RoutedEventArgs e)
+    {
+        if (tbCalculator.Text.Length > 0)
         {
-            InitializeComponent();
+            tbCalculator.Text = tbCalculator.Text.Substring(0, tbCalculator.Text.Length - 1);
+        }
+    }
+
+    private void btnReturnProduct_Click(object sender, RoutedEventArgs e)
+    {
+        ReturnProductWindow returnProductWindow = new ReturnProductWindow();
+        returnProductWindow.ShowDialog();
+    }
+
+    private void btnPay_Click(object sender, RoutedEventArgs e)
+    {
+        PaymentTypeWindow paymentTypeWindow = new PaymentTypeWindow();
+        paymentTypeWindow.ShowDialog();
+    }
+
+    public void GetData()
+    {
+        tbDate.Text = DateTime.UtcNow.Month + "." + DateTime.UtcNow.Day + "." + DateTime.UtcNow.Year;
+        tbhour.Text = DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+    }
+
+    private void vaqt_ketdi(object sender, ElapsedEventArgs e)
+    {
+        this.Dispatcher.Invoke(() =>
+        {
+            if (tbhour.Opacity == 1)
+            {
+                tbhour.Opacity = 1;
+            }
+            else
+                tbhour.Opacity = 0.5;
             GetData();
-            timer.Elapsed += vaqt_ketdi;
-            timer.Interval = 500;
-            timer.Enabled = true;
-        }
+        });
+    }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+    private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        activeTextboxIndex = int.Parse(((TextBox)sender).Uid);
+    }
+
+    private void WriteNumber(string number)
+    {
+        if (activeTextboxIndex == 2)
         {
-
-            var button = (Button)sender;
-            WriteNumber(button.Content.ToString() ?? "");
-
+            tbCalculator.Text = tbCalculator.Text.ToString() + number;
         }
+    }
 
-        private void btnClear_Click(object sender, RoutedEventArgs e)
+    private void Page_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+    {
+        barcode = e.Text;
+
+        if (string.IsNullOrEmpty(barcode))
         {
-            tbCalculator.Text = string.Empty;
+            Label lblBarcode = new Label();
+            lblBarcode.Content = barcode.ToString();
+            lblBarcode.FontSize = 16;
+            lblBarcode.HorizontalAlignment = HorizontalAlignment.Center;
+
+            St_product.Children.Add(lblBarcode);
+            barcode = "";
         }
+    }
 
-        private void btnBackKlav_Click(object sender, RoutedEventArgs e)
-        {
-            if (tbCalculator.Text.Length > 0)
-            {
-                tbCalculator.Text = tbCalculator.Text.Substring(0, tbCalculator.Text.Length - 1);
-            }
-        }
+    private void Nasiya_Button_Click(object sender, RoutedEventArgs e)
+    {
 
-        private void btnReturnProduct_Click(object sender, RoutedEventArgs e)
-        {
-            ReturnProductWindow returnProductWindow = new ReturnProductWindow();
-            returnProductWindow.ShowDialog();
-        }
-
-        private void btnPay_Click(object sender, RoutedEventArgs e)
-        {
-            PaymentTypeWindow paymentTypeWindow = new PaymentTypeWindow();
-            paymentTypeWindow.ShowDialog();
-        }
-
-        public void GetData()
-        {
-            tbDate.Text = DateTime.UtcNow.Month + "." + DateTime.UtcNow.Day + "." + DateTime.UtcNow.Year;
-            tbhour.Text = DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
-        }
-
-        private void vaqt_ketdi(object sender, ElapsedEventArgs e)
-        {
-            this.Dispatcher.Invoke(() =>
-            {
-                if (tbhour.Opacity == 1)
-                {
-                    tbhour.Opacity = 1;
-                }
-                else
-                    tbhour.Opacity = 0.5;
-                GetData();
-            });
-        }
-
-        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-            activeTextboxIndex = int.Parse(((TextBox)sender).Uid);
-        }
-
-        private void WriteNumber(string number)
-        {
-            if (activeTextboxIndex == 2)
-            {
-                tbCalculator.Text = tbCalculator.Text.ToString() + number;
-            }
-        }
-
-        private void Page_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
-        {
-            barcode = e.Text;
-
-            if (string.IsNullOrEmpty(barcode))
-            {
-                Label lblBarcode = new Label();
-                lblBarcode.Content = barcode.ToString();
-                lblBarcode.FontSize = 16;
-                lblBarcode.HorizontalAlignment = HorizontalAlignment.Center;
-
-                St_product.Children.Add(lblBarcode);
-                barcode = "";
-            }
-        }
     }
 }
