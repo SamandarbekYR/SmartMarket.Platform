8326fc7c7c7bcab2571e38b54583423e2549ec52
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-09-21 23:09:32 +0500

    Tablet boshlandi

---
+++ src/SmartMarket.Desktop.Tablet/Windows/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace SmartMarket.Desktop.Tablet;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Close_Button_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
