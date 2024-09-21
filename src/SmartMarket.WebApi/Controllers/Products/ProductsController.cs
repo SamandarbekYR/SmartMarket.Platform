64436fb2b1cc4178fccfd4f44b4a615e355745fc
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-09-21 06:19:01 +0500

    update

--- src/SmartMarket.WebApi/Controllers/Products/ProductsController.cs
+++ src/SmartMarket.WebApi/Controllers/Products/ProductsController.cs
@@ -1,26 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartMarket.Service.Common.Exceptions;
 using SmartMarket.Service.DTOs.Products.Product;
 using SmartMarket.Service.Interfaces.Products.Product;
+using SmartMarket.WebApi.Controllers.Common;
 
 namespace SmartMarket.WebApi.Controllers.Products
 {
     [Route("api/products")]
     [ApiController]
-    public class ProductsController(IProductService productService) : ControllerBase
+    public class ProductsController(IProductService productService) : BaseController
     {
         private readonly IProductService _productService = productService;
 
         [HttpGet]
-        public async Task<IActionResult> GetAllAsync()
-        {
-            var products = await _productService.GetAllAsync();
-            return Ok(products);
-        }
+        public async Task<IActionResult> GetAllAsync([FromQuery] PaginationParams @params)
+            => Ok(await _productService.GetAllAsync(@params));
 
         [HttpPost]
         public async Task<IActionResult> AddAsync([FromForm] AddProductDto dto)
         {
-            var productId = await _productService.AddAsync(dto); 
+            var productId = await _productService.AddAsync(dto);
             return Ok(productId);
         }
 
@@ -36,6 +35,76 @@ namespace SmartMarket.WebApi.Controllers.Products
         {
             await _productService.UpdateAsync(dto, id);
             return Ok();
+        }
+
+        [HttpGet("barcode/{barcode}")]
+        public async Task<IActionResult> GetProductByBarcodeAsync(string barcode)
+        {
+            try
+            {
+                var product = await _productService.GetProductByBarcodeAsync(barcode);
+                return Ok(product);
+            }
+            catch (StatusCodeException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("pcode/{pCode}")]
+        public async Task<IActionResult> GetProductByPCodeAsync(string pCode)
+        {
+            try
+            {
+                var product = await _productService.GetProductByPCodeAsync(pCode);
+                return Ok(product);
+            }
+            catch (StatusCodeException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("worker/{workerId}")]
+        public async Task<IActionResult> GetProductByWorkerAsync(Guid workerId)
+        {
+            try
+            {
+                var product = await _productService.GetProductByWorkerIdAsync(workerId);
+                return Ok(product);
+            }
+            catch (StatusCodeException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        public async Task<IActionResult> SearchProductsAsync([FromQuery] string searchTerm, [FromQuery] PaginationParams @params)
+        {
+            try
+            {
+                var products = await _productService.SearchProductsAsync(searchTerm, @params);
+                return Ok(products);
+            }
+            catch (StatusCodeException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
