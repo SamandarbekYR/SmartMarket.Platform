fe42f6b9e9c752c46b5e6584a706ebd6080ffb04
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-09-24 16:32:12 +0500

    LoginWindow

--- src/SmartMarket.Desktop.Tablet/Pages/MainPage.xaml.cs
+++ src/SmartMarket.Desktop.Tablet/Pages/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SmartMarket.Desktop.Tablet.Components;
+using SmartMarket.Desktop.Tablet.Windows;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -50,5 +51,14 @@ public partial class MainPage : Page
         SecondPage secondPage = new SecondPage();
         MainWindow mainWindow = GetMainWindow();
         mainWindow.PageNavigator.Content = secondPage;
+    }
+
+    private void Logout_Click(object sender, RoutedEventArgs e)
+    {
+        LoginWindow loginWindow = new LoginWindow();
+        MainWindow mainWindow = GetMainWindow();
+
+        mainWindow.Close();
+        loginWindow.ShowDialog();
     }
 }
---
+++ src/SmartMarket.Desktop.Tablet/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace SmartMarket.Desktop.Tablet.Windows;
+
+/// <summary>
+/// Interaction logic for LoginWindow.xaml
+/// </summary>
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void Login_Button_Click(object sender, RoutedEventArgs e)
+    {
+        MainWindow mainWindow = new MainWindow();
+        this.Close();
+        mainWindow.ShowDialog();
+    }
+}
