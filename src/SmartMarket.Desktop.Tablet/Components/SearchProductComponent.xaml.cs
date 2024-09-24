2467ac8f25cca01c4d53430dac5ab1ec706abaee
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-09-24 17:14:57 +0500

    Added QuantityWindow

--- src/SmartMarket.Desktop.Tablet/Components/SearchProductComponent.xaml.cs
+++ src/SmartMarket.Desktop.Tablet/Components/SearchProductComponent.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SmartMarket.Desktop.Tablet.Windows;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SmartMarket.Desktop.Tablet.Components
+namespace SmartMarket.Desktop.Tablet.Components;
+
+/// <summary>
+/// Interaction logic for SearchProductComponent.xaml
+/// </summary>
+public partial class SearchProductComponent : UserControl
 {
-    /// <summary>
-    /// Interaction logic for SearchProductComponent.xaml
-    /// </summary>
-    public partial class SearchProductComponent : UserControl
+    public SearchProductComponent()
     {
-        public SearchProductComponent()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void Add_Button_Click(object sender, RoutedEventArgs e)
+    {
+        QuantityWindow quantityWindow = new QuantityWindow();
+        quantityWindow.ShowDialog();
     }
 }
