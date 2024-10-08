51dd3ac9224998dc9c7b69e1b091826ebdfa0ec8
MrUstoz <abdulvosidfoziljonov55@gmail.com>
2024-10-08 23:09:00 +0500

    add expense page get server

---
+++ src/SmartMarket.Desktop/Components/ExpenseForComponents/ExpenseComponent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartMarket.Desktop.Components.ExpenseForComponents
+{
+    /// <summary>
+    /// Interaction logic for ExpenseComponent.xaml
+    /// </summary>
+    public partial class ExpenseComponent : UserControl
+    {
+        public ExpenseComponent()
+        {
+            InitializeComponent();
+        }
+    }
+}
