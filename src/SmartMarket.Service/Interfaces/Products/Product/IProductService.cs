f1325a959b24a4040ddc77dc75d3bc2060b8a9ff
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-10-12 09:55:29 +0500

    fx

--- src/SmartMarket.Service/Interfaces/Products/Product/IProductService.cs
+++ src/SmartMarket.Service/Interfaces/Products/Product/IProductService.cs
@@ -8,7 +8,7 @@ namespace SmartMarket.Service.Interfaces.Products.Product
     {
         Task<Guid> AddAsync(AddProductDto dto);
         Task<bool> DeleteAsync(Guid Id);
-        Task<IEnumerable<FullProductDto>> GetAllAsync(PaginationParams @params);
+        Task<IList<FullProductDto>> GetAllAsync(PaginationParams @params);
         Task<bool> UpdateAsync(AddProductDto dto, Guid Id);
         Task<bool> SellProductAsync(string barcode);
         Task<IEnumerable<ProductDto>> GetProductsByCategoryIdAsync(Guid categoryId, PaginationParams @params);
