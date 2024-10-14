b21f4df15fa91c7c7f3e583edd1f8515f83fbf9c
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-10-14 15:23:55 +0500

    update

---
+++ src/SmartMarketDeskop.Integrated/ViewModelsForUI/Products/LoadReportViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartMarketDeskop.Integrated.ViewModelsForUI.Products
+{
+    public class LoadReportViewModel
+    {
+        public string PCode { get; set; } = string.Empty;
+        public string Barcode { get; set; } = string.Empty;
+        public string ProductName { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+        public string WorkerName { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public double SellPrice { get; set; }
+        public int Count { get; set; }
+        public string UnitOfMeasure { get; set; } = string.Empty;
+        public double TotalPrice { get; set; }
+        public DateTime Date { get; set; }
+        public string ManuFacture { get; set; } = string.Empty;
+    }
+}
