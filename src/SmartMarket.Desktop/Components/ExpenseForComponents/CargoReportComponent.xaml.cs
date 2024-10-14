659aca4c39a69815c6547e14e703448b5d3b1fef
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-10-14 16:39:38 +0500

    CargoReport

--- src/SmartMarket.Desktop/Components/ExpenseForComponents/CargoReportComponent.xaml.cs
+++ src/SmartMarket.Desktop/Components/ExpenseForComponents/CargoReportComponent.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartMarket.Service.DTOs.Products.LoadReport;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace SmartMarket.Desktop.Components.ExpenseForComponents
         public CargoReportComponent()
         {
             InitializeComponent();
+        }
+
+        public void SetData(LoadReportDto dto)
+        {
+
         }
     }
 }
