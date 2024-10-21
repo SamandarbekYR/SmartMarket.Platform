02bef4b53b2534e8a03deabee198420fec185fb7
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-10-21 18:00:03 +0500

    AccountSettingsPage update

--- src/SmartMarket.Desktop/Components/AccountSettingsForComponent/AccountSettingsComponent.xaml.cs
+++ src/SmartMarket.Desktop/Components/AccountSettingsForComponent/AccountSettingsComponent.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using SmartMarket.Desktop.Windows.AccountSettings;
 using SmartMarket.Service.DTOs.Workers.Worker;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SmartMarket.Desktop.Components.AccountSettingsForComponent
 {
@@ -29,17 +16,17 @@ namespace SmartMarket.Desktop.Components.AccountSettingsForComponent
         }
 
 
-        public void SetData(string firstName,string lastName, string position)
+        public void SetData(WorkerDto dto)
         {
-            lbName.Text = firstName + "  " + lastName;
-            lbPositon.Text = position;
+            lbName.Text = dto.FirstName + "  " + dto.LastName;
+            lbPositon.Text = dto.Position.Name;
         }
 
         private void Border_MouseUp(object sender, MouseButtonEventArgs e)
         {
             var worker = this.Tag as WorkerDto;
 
-            AccountUpdateWindow accountUpdateWindow = new AccountUpdateWindow(worker);
+            AccountUpdateWindow accountUpdateWindow = new AccountUpdateWindow(worker!);
             accountUpdateWindow.ShowDialog();   
         }
     }
