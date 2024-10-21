a7668f904251ee7ebf5f79b24ec10b00b47b3376
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-10-21 12:07:25 +0500

    Update

--- src/SmartMarket.Desktop/Components/ExpenseForComponents/RunningProductComponent.xaml.cs
+++ src/SmartMarket.Desktop/Components/ExpenseForComponents/RunningProductComponent.xaml.cs
@@ -1,44 +1,30 @@
 ﻿using SmartMarket.Service.DTOs.Products.Product;
 using SmartMarketDeskop.Integrated.Services.Products.Product;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SmartMarket.Desktop.Components.ExpenseForComponents
+namespace SmartMarket.Desktop.Components.ExpenseForComponents;
+
+/// <summary>
+/// Interaction logic for RunningProductComponent.xaml
+/// </summary>
+public partial class RunningProductComponent : UserControl
 {
-    /// <summary>
-    /// Interaction logic for RunningProductComponent.xaml
-    /// </summary>
-    public partial class RunningProductComponent : UserControl
+    private readonly IProductService productService;
+    public RunningProductComponent()
     {
-        private readonly IProductService productService;
-        public RunningProductComponent()
-        {
-            InitializeComponent();
-            this.productService = new ProductService();
-        }
-        public Guid ProductId { get; set; }
+        InitializeComponent();
+        this.productService = new ProductService();
+    }
+    public Guid ProductId { get; set; }
 
-        public void SetData(ProductDto dto)
-        {
-            var imgPath = @"D:\SmartPartnersProjects\src\SmartMarket.Desktop\Assets\Basket.png";
+    public void SetData(FullProductDto dto)
+    {
+        var imgPath = @"D:\SmartPartnersProjects\src\SmartMarket.Desktop\Assets\Basket.png";
 
-            tbName.Content = dto.Name;
-            tbCount.Content = dto.Count;
-            tbPrice.Content = dto.Price;
-            productImg.Source = new BitmapImage(new Uri(imgPath, UriKind.Absolute)); //imgPath dto yoqligi uchun testga localdan
-        }
+        tbName.Content = dto.Name;
+        tbCount.Content = dto.Count;
+        tbPrice.Content = dto.Price;
+        productImg.Source = new BitmapImage(new Uri(imgPath, UriKind.Absolute)); //imgPath dto yoqligi uchun testga localdan
     }
 }
