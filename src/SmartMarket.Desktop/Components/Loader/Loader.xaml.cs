fac116ce4036864cc4b62d6481050a8c172ca5da
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-10-21 23:11:01 +0500

    Added Loader

---
+++ src/SmartMarket.Desktop/Components/Loader/Loader.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace SmartMarket.Desktop.Components.Loader;
+
+/// <summary>
+/// Interaction logic for Loader.xaml
+/// </summary>
+public partial class Loader : UserControl
+{
+    public Loader()
+    {
+        InitializeComponent();
+    }
+}
