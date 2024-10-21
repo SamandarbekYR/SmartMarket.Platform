2486ed8c47e5924850dba4e77c3dc4c5af3a734b
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-10-21 17:20:43 +0500

    ShopDetailsPage update

--- src/SmartMarket.Desktop/Pages/ShopDetailsForPage/TopSalePage.xaml.cs
+++ src/SmartMarket.Desktop/Pages/ShopDetailsForPage/TopSalePage.xaml.cs
@@ -1,147 +1,156 @@
 ﻿using SmartMarket.Desktop.Components.ShopDetailsForComponent;
 using SmartMarket.Service.DTOs.Products.ProductSale;
 using SmartMarket.Service.ViewModels.Products;
-
 using SmartMarketDeskop.Integrated.Services.Products.ProductSale;
-
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
-namespace SmartMarket.Desktop.Pages.ShopDetailsForPage
+namespace SmartMarket.Desktop.Pages.ShopDetailsForPage;
+
+/// <summary>
+/// Interaction logic for TopSalePage.xaml
+/// </summary>
+public partial class TopSalePage : Page
 {
-    /// <summary>
-    /// Interaction logic for TopSalePage.xaml
-    /// </summary>
-    public partial class TopSalePage : Page
+    private readonly IProductSaleService _productSaleService;
+    public TopSalePage()
     {
-        private readonly IProductSaleService _productSaleService;
-        public TopSalePage()
+        InitializeComponent();
+        _productSaleService = new ProductSaleService();
+    }
+
+    public async void GetAllTopSale()
+    {
+        St_TopSale.Children.Clear();
+
+        var topSaleProducts = await Task.Run(async () => await _productSaleService.GetAllAsync());
+
+        var today = DateTime.Today;
+        var filteredTopSales = topSaleProducts.Where(ps => ps.CreatedDate!.Value.Date == today).ToList();
+
+        await ShowProductSales(filteredTopSales);
+    }
+
+    private async void FilterProductSales()
+    {
+        St_TopSale.Children.Clear();
+
+        FilterProductSaleDto filterProductSaleDto = new FilterProductSaleDto();
+
+        if (fromDateTime.SelectedDate != null && toDateTime.SelectedDate != null)
         {
-            InitializeComponent();
-            _productSaleService = new ProductSaleService();
+            filterProductSaleDto.FromDateTime = fromDateTime.SelectedDate.Value;
+            filterProductSaleDto.ToDateTime = toDateTime.SelectedDate.Value;
         }
 
-        public async void GetALlTopSale()
+        var searchTerm = searchTextBox.Text.ToLower();
+        if (!string.IsNullOrEmpty(searchTerm))
         {
-            var topSaleProducts = await _productSaleService.GetAllAsync();
-            var today = DateTime.Today;
-            topSaleProducts = topSaleProducts.Where(ps => ps.CreatedDate.Value.Date == today).ToList();
-            ShowProductSales(topSaleProducts);
+            filterProductSaleDto.ProductName = searchTerm;
         }
 
-        private async void FilterProductSales()
+        var filteredProductSales = await Task.Run(async () => await _productSaleService.FilterProductSaleAsync(filterProductSaleDto));
+
+        await ShowProductSales(filteredProductSales);
+    }
+
+    private async Task ShowProductSales(IEnumerable<ProductSaleViewModel> productSales)
+    {
+        productSales = productSales.Where(ps => ps.Count != 0)
+            .OrderByDescending(ps => ps.CreatedDate).ToList();
+
+        var groupedProducts = productSales
+            .GroupBy(p => p.Product.Name)
+            .Select(group => new
+            {
+                ProductName = group.Key,
+                TotalCount = group.Sum(p => p.Count), // Mahsulotning umumiy sotilgan soni
+                TotalSales = group.Sum(p => p.Product.SellPrice * p.Count), // Jami sotuv summasi
+                Profit = group.Sum(p => (p.Product.SellPrice - p.Product.Price) * p.Count) // Foyda summasi
+            }).OrderByDescending(p => p.TotalCount).ToList();
+
+        // Foydalanuvchi tomonidan kiritilgan son bo'yicha eng ko'p sotilganlarni olish
+        if (!string.IsNullOrEmpty(countTextBox.Text))
         {
-            FilterProductSaleDto filterProductSaleDto = new FilterProductSaleDto();
-
-            if (fromDateTime.SelectedDate != null && toDateTime.SelectedDate != null)
+            int count;
+            if (int.TryParse(countTextBox.Text, out count))
             {
-                filterProductSaleDto.FromDateTime = fromDateTime.SelectedDate.Value;
-                filterProductSaleDto.ToDateTime = toDateTime.SelectedDate.Value;
-            }
-
-            var searchTerm = searchTextBox.Text.ToLower();
-            if (!string.IsNullOrEmpty(searchTerm))
-            {
-                filterProductSaleDto.ProductName = searchTerm;
-            }
-
-            var filteredProductSales = await _productSaleService.FilterProductSaleAsync(filterProductSaleDto);
-
-            ShowProductSales(filteredProductSales);
-        }
-
-        private void ShowProductSales(IEnumerable<ProductSaleViewModel> productSales)
-        {
-            productSales = productSales.Where(ps => ps.Count != 0)
-                .OrderByDescending(ps => ps.CreatedDate).ToList();
-
-            var groupedProducts = productSales
-                .GroupBy(p => p.Product.Name)
-                .Select(group => new
-                {
-                    ProductName = group.Key,
-                    TotalCount = group.Sum(p => p.Count), // Mahsulotning umumiy sotilgan soni
-                    TotalSales = group.Sum(p => p.Product.SellPrice * p.Count), // Jami sotuv summasi
-                    Profit = group.Sum(p => (p.Product.SellPrice - p.Product.Price) * p.Count) // Foyda summasi
-                }).OrderByDescending(p => p.TotalCount).ToList();
-
-            if(!string.IsNullOrEmpty(countTextBox.Text))
-            {
-                int count = int.Parse(countTextBox.Text);
                 groupedProducts = groupedProducts.Take(count).ToList();
             }
-
-            St_TopSale.Visibility = Visibility.Visible;
-            St_TopSale.Children.Clear();
-            int rowNumber = 1;
-
-            foreach (var item in groupedProducts)
-            {
-                TopSaleComponent topSaleComponent = new TopSaleComponent();
-                topSaleComponent.Tag = rowNumber;
-                topSaleComponent.SetValues(
-                    item.ProductName,
-                    item.TotalCount,
-                    item.TotalSales,
-                    item.Profit);
-
-                topSaleComponent.BorderThickness = new Thickness(2);
-                St_TopSale.Children.Add(topSaleComponent);
-                rowNumber++;
-            }
         }
 
-        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        St_TopSale.Visibility = Visibility.Visible;
+        St_TopSale.Children.Clear();
+        int rowNumber = 1;
+
+        foreach (var item in groupedProducts)
+        {
+            TopSaleComponent topSaleComponent = new TopSaleComponent();
+            topSaleComponent.Tag = rowNumber;
+            topSaleComponent.SetValues(
+                item.ProductName,
+                item.TotalCount,
+                item.TotalSales,
+                item.Profit);
+
+            topSaleComponent.BorderThickness = new Thickness(2);
+            St_TopSale.Children.Add(topSaleComponent);
+            rowNumber++;
+        }
+    }
+
+    private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+    {
+        FilterProductSales();
+    }
+
+    private async void CountTextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            var topSaleProducts = await Task.Run(async () => await _productSaleService.GetAllAsync());
+            await ShowProductSales(topSaleProducts);
+        }
+    }
+
+    private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
         {
             FilterProductSales();
         }
+    }
 
-        private async void CountTextBox_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                var topSaleProducts = await _productSaleService.GetAllAsync();
-                ShowProductSales(topSaleProducts);
-            }
-        }
+    private void CountTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+    {
+        e.Handled = !IsTextAllowed(e.Text);
+    }
 
-        private void SearchTextBox_KeyDown(object sender, KeyEventArgs e)
+    private void CountTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
+    {
+        if (e.DataObject.GetDataPresent(typeof(string)))
         {
-            if (e.Key == Key.Enter)
-            {
-                FilterProductSales();
-            }
-        }
-        private void CountTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
-        {
-            e.Handled = !IsTextAllowed(e.Text);
-        }
-
-        private void CountTextBox_Pasting(object sender, DataObjectPastingEventArgs e)
-        {
-            if (e.DataObject.GetDataPresent(typeof(string)))
-            {
-                string text = (string)e.DataObject.GetData(typeof(string));
-                if (!IsTextAllowed(text))
-                {
-                    e.CancelCommand(); 
-                }
-            }
-            else
+            string text = (string)e.DataObject.GetData(typeof(string));
+            if (!IsTextAllowed(text))
             {
                 e.CancelCommand();
             }
         }
-
-        private static bool IsTextAllowed(string text)
+        else
         {
-            return text.All(char.IsDigit); 
-        }
-
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            GetALlTopSale();
+            e.CancelCommand();
         }
     }
+
+    private static bool IsTextAllowed(string text)
+    {
+        return text.All(char.IsDigit); // Faqat raqamlar kiritish mumkin
+    }
+
+    private void Page_Loaded(object sender, RoutedEventArgs e)
+    {
+        GetAllTopSale();
+    }
+
 }
