25b75f17271db2be3c5190c4e3e46c49eb721dd0
Bahriddin Abdusalomov <b.abdusalomov@baik.uz>
2024-11-08 17:13:24 +0500

    partners payment

--- src/SmartMarket.DataAccess/Interfaces/Partners/IPartner.cs
+++ src/SmartMarket.DataAccess/Interfaces/Partners/IPartner.cs
@@ -1,7 +1,10 @@
 ﻿using SmartMarket.Domain.Entities.Partners;
+using SmartMarket.Domain.Entities.Workers;
 
 namespace SmartMarket.DataAccess.Interfaces.Partners
 {
     public interface IPartner : IRepository<Partner>
-    { }
+    {
+        public Task<List<Partner>> GetPartnersFullInformationAsync();
+    }
 }
--- src/SmartMarket.DataAccess/Repositories/Partners/PartnerRepository.cs
+++ src/SmartMarket.DataAccess/Repositories/Partners/PartnerRepository.cs
@@ -1,12 +1,28 @@
-﻿using SmartMarket.DataAccess.Data;
+﻿using Microsoft.EntityFrameworkCore;
+
+using SmartMarket.DataAccess.Data;
 using SmartMarket.DataAccess.Interfaces.Partners;
 using SmartMarket.Domain.Entities.Partners;
+using SmartMarket.Domain.Entities.Workers;
 
 namespace SmartMarket.DataAccess.Repositories.Partners
 {
     public class PartnerRepository : Repository<Partner>, IPartner
     {
+        private readonly AppDbContext _appDbContext;
+        private DbSet<Partner> _partners;
         public PartnerRepository(AppDbContext appDb) : base(appDb)
-        { }
+        { 
+            _appDbContext = appDb;
+            _partners = appDb.Set<Partner>();
+        }
+
+        public async Task<List<Partner>> GetPartnersFullInformationAsync()
+        {
+            return await _partners
+                .Include(p => p.Debtors)
+                    .ThenInclude(d => d.DebtPayment)
+                .ToListAsync();
+        }
     }
 }
