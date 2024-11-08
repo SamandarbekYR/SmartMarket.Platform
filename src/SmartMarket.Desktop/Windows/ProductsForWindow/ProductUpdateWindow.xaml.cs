318493b0582345b3cea8390a53835419bfd55b1e
ShodiyorErqoziyev <shodiyor918@gmail.com>
2024-11-08 19:16:26 +0500

    fx

--- src/SmartMarket.Desktop/Windows/ProductsForWindow/ProductUpdateWindow.xaml.cs
+++ src/SmartMarket.Desktop/Windows/ProductsForWindow/ProductUpdateWindow.xaml.cs
@@ -23,214 +23,213 @@ using ToastNotifications.Position;
 using static SmartMarket.Desktop.Windows.BlurWindow.BlurEffect;
 using S = SmartMarket.Service.DTOs.Products.Product;
 
-namespace SmartMarket.Desktop.Windows.ProductsForWindow
+namespace SmartMarket.Desktop.Windows.ProductsForWindow;
+
+/// <summary>
+/// Interaction logic for ProductUpdateWindow.xaml
+/// </summary>
+public partial class ProductUpdateWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for ProductUpdateWindow.xaml
-    /// </summary>
-    public partial class ProductUpdateWindow : Window
+    private ICategoryService categoryService;
+    private IContrAgentService contrAgentService;
+    private IProductService productService;
+    private IProductImageService productImageService;
+
+
+    List<CategoryView> categories = new List<CategoryView>();
+    List<ContrAgentViewModels> contrAgents = new List<ContrAgentViewModels>();
+    private string imagepath;
+    private Guid productId = Guid.Empty;
+    private Guid workerId = Guid.Empty;
+
+    public ProductUpdateWindow()
     {
-        private ICategoryService categoryService;
-        private IContrAgentService contrAgentService;
-        private IProductService productService;
-        private IProductImageService productImageService;
+        InitializeComponent();
+        categoryService = new CategoryService();
+        contrAgentService = new ContrAgentService();
+        productService = new ProductService();
+        productImageService = new ProductImageService();
+    }
 
+    [DllImport("user32.dll")]
+    internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+    internal void EnableBlur()
+    {
+        var windowHelper = new WindowInteropHelper(this);
 
-        List<CategoryView> categories = new List<CategoryView>();
-        List<ContrAgentViewModels> contrAgents = new List<ContrAgentViewModels>();
-        private string imagepath;
-        private Guid productId = Guid.Empty;
-        private Guid workerId = Guid.Empty;
+        var accent = new AccentPolicy();
+        accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
 
-        public ProductUpdateWindow()
+        var accentStructSize = Marshal.SizeOf(accent);
+
+        var accentPtr = Marshal.AllocHGlobal(accentStructSize);
+        Marshal.StructureToPtr(accent, accentPtr, false);
+
+        var data = new WindowCompositionAttributeData();
+        data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
+        data.SizeOfData = accentStructSize;
+        data.Data = accentPtr;
+
+        SetWindowCompositionAttribute(windowHelper.Handle, ref data);
+
+        Marshal.FreeHGlobal(accentPtr);
+    }
+
+    Notifier notifier = new Notifier(cfg =>
+    {
+        cfg.PositionProvider = new WindowPositionProvider(
+            parentWindow: Application.Current.MainWindow,
+            corner: Corner.TopRight,
+            offsetX: 20,
+            offsetY: 20);
+
+        cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+            notificationLifetime: TimeSpan.FromSeconds(3),
+            maximumNotificationCount: MaximumNotificationCount.FromCount(2));
+
+        cfg.Dispatcher = Application.Current.Dispatcher;
+    });
+
+    public void SetData(S.FullProductDto product)
+    {
+        productId = product.Id;
+        workerId = product.WorkerId;
+        txtBarCode.Text = product.Barcode.ToString();
+        txtProductName.Text = product.Name;
+        comboCategory.SelectedValue = product.CategoryName;
+        txtQuantity.Text = product.Count.ToString();
+        txtPrice.Text = product.Price.ToString();
+        txtProductPriceSum.Text = product.SellPrice.ToString();
+        int persentage = (int)((product.SellPrice - product.Price) * 100 / product.Price);
+        comboMeasurement.Text = product.UnitOfMeasure;
+        comboDelivery.Text = product.WorkerFirstName;
+        txtNoteAmount.Text = product.NoteAmount.ToString();
+    }
+
+    private async void btnUpdate_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        AddProductDto addProductDto = new AddProductDto();
+
+        addProductDto.BarCode = txtBarCode.Text;
+        addProductDto.ProductName = txtProductName.Text;
+
         {
-            InitializeComponent();
-            categoryService = new CategoryService();
-            contrAgentService = new ContrAgentService();
-            productService = new ProductService();
-            productImageService = new ProductImageService();
+            CategoryView categoryView = categories.Where(a => a.Name == comboCategory.SelectedValue.ToString()).FirstOrDefault()!;
+            addProductDto.CategoryId = categoryView.Id;
         }
 
-        [DllImport("user32.dll")]
-        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
-        internal void EnableBlur()
+        addProductDto.Count = int.Parse(txtQuantity.Text);
+        addProductDto.Price = double.Parse(txtPrice.Text);
+        addProductDto.SellPrice = double.Parse(txtProductPriceSum.Text);
+
+        addProductDto.UnitOfMeasure = comboMeasurement.Text;
         {
-            var windowHelper = new WindowInteropHelper(this);
-
-            var accent = new AccentPolicy();
-            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
-
-            var accentStructSize = Marshal.SizeOf(accent);
-
-            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
-            Marshal.StructureToPtr(accent, accentPtr, false);
-
-            var data = new WindowCompositionAttributeData();
-            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
-            data.SizeOfData = accentStructSize;
-            data.Data = accentPtr;
-
-            SetWindowCompositionAttribute(windowHelper.Handle, ref data);
-
-            Marshal.FreeHGlobal(accentPtr);
+            ContrAgentViewModels contrAgentViewModels = contrAgents.Where(a => a.FirstName == comboDelivery.SelectedValue).FirstOrDefault()!;
+            addProductDto.ContrAgentId = contrAgentViewModels.Id;
         }
 
-        Notifier notifier = new Notifier(cfg =>
-        {
-            cfg.PositionProvider = new WindowPositionProvider(
-                parentWindow: Application.Current.MainWindow,
-                corner: Corner.TopRight,
-                offsetX: 20,
-                offsetY: 20);
-
-            cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
-                notificationLifetime: TimeSpan.FromSeconds(3),
-                maximumNotificationCount: MaximumNotificationCount.FromCount(2));
-
-            cfg.Dispatcher = Application.Current.Dispatcher;
-        });
-
-        public void SetData(S.FullProductDto product)
-        {
-            productId = product.Id;
-            workerId = product.WorkerId;
-            txtBarCode.Text = product.Barcode.ToString();
-            txtProductName.Text = product.Name;
-            comboCategory.SelectedValue = product.CategoryName;
-            txtQuantity.Text = product.Count.ToString();
-            txtPrice.Text = product.Price.ToString();
-            txtProductPriceSum.Text = product.SellPrice.ToString();
-            int persentage = (int)((product.SellPrice - product.Price) * 100 / product.Price);
-            comboMeasurement.Text = product.UnitOfMeasure;
-            comboDelivery.Text = product.WorkerFirstName;
-            txtNoteAmount.Text = product.NoteAmount.ToString();
-        }
-
-        private async void btnUpdate_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            AddProductDto addProductDto = new AddProductDto();
-
-            addProductDto.BarCode = txtBarCode.Text;
-            addProductDto.ProductName = txtProductName.Text;
-
-            {
-                CategoryView categoryView = categories.Where(a => a.Name == comboCategory.SelectedValue.ToString()).FirstOrDefault()!;
-                addProductDto.CategoryId = categoryView.Id;
-            }
-
-            addProductDto.Count = int.Parse(txtQuantity.Text);
-            addProductDto.Price = double.Parse(txtPrice.Text);
-            addProductDto.SellPrice = double.Parse(txtProductPriceSum.Text);
-
-            addProductDto.UnitOfMeasure = comboMeasurement.Text;
-            {
-                ContrAgentViewModels contrAgentViewModels = contrAgents.Where(a => a.FirstName == comboDelivery.SelectedValue).FirstOrDefault()!;
-                addProductDto.ContrAgentId = contrAgentViewModels.Id;
-            }
-
-            addProductDto.WorkerId = workerId;
-            addProductDto.PaymentStatus = "Active";
-            addProductDto.NoteAmount = int.Parse(txtNoteAmount.Text);
-            var res = await Task.Run(async () => await productService.UpdateProduct(addProductDto, productId));
+        addProductDto.WorkerId = workerId;
+        addProductDto.PaymentStatus = "Active";
+        addProductDto.NoteAmount = int.Parse(txtNoteAmount.Text);
+        var res = await Task.Run(async () => await productService.UpdateProduct(addProductDto, productId));
 
 
-            ProductImageDto productImageDto = new ProductImageDto();
-            productImageDto.ImagePath = imagepath;
+        ProductImageDto productImageDto = new ProductImageDto();
+        productImageDto.ImagePath = imagepath;
 
-            if (res)
-            {
-                this.Close();
-                notifier.ShowInformation("Mahsulot muvaffaqiyatli o'zgartirildi.");
-            }
-            else
-            {
-                notifier.ShowError("Mahsulot yaratishda xato yuz berdi.");
-            }
-        }
-
-        private void btnClear_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            Clear();
-        }
-
-        private void phone_number_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            TextBox textBox = (TextBox)sender;
-            string text = textBox.Text;
-            string filteredText = Regex.Replace(text, "[^0-9]+", "");
-
-            if (text != filteredText)
-            {
-                int caretIndex = textBox.CaretIndex;
-                textBox.Text = filteredText;
-                textBox.CaretIndex = caretIndex > 0 ? caretIndex - 1 : 0;
-            }
-        }
-
-        private void btnCreateCategory_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            CategoryCreateWindow categoryCreate = new CategoryCreateWindow();
-            categoryCreate.ShowDialog();
-        }
-
-        private void btnCreateContrAgent_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            ContrAgentCreateWindow contrAgentCreate = new ContrAgentCreateWindow();
-            contrAgentCreate.ShowDialog();
-        }
-
-        public async Task GetAllCategory()
-        {
-            categories = await Task.Run(async () => await categoryService.GetAllAsync());
-            if (categories != null && categories.Any())
-            {
-                comboCategory.ItemsSource = categories.Select(a => a.Name);
-                comboCategory.Items.Refresh();
-            }
-        }
-
-        public async Task GetAllContrAgent()
-        {
-            contrAgents = await Task.Run(async () => await contrAgentService.GetAll());
-            if (contrAgents != null && contrAgents.Any())
-            {
-                comboDelivery.ItemsSource = contrAgents.Select(a => a.FirstName);
-                comboDelivery.Items.Refresh();
-            }
-        }
-
-        public void Clear()
-        {
-            txtBarCode.Text = txtProductName.Text = comboCategory.Text = txtQuantity.Text = txtPrice.Text =
-            txtProductPriceSum.Text = comboDelivery.Text = txtNoteAmount.Text = string.Empty;
-        }
-
-        private void BrClose_MouseUp(object sender, MouseButtonEventArgs e)
+        if (res)
         {
             this.Close();
-            Clear();
+            notifier.ShowInformation("Mahsulot muvaffaqiyatli o'zgartirildi.");
         }
-
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        else
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "JPG Files (*.jpg)|*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png";
-            if (openFileDialog.ShowDialog() == true)
-            {
-                imagepath = openFileDialog.FileName;
-                BitmapImage bitmap = new BitmapImage();
-                bitmap.BeginInit();
-                bitmap.UriSource = new Uri(imagepath, UriKind.Absolute);
-                bitmap.EndInit();
-                lbImage.Content = Path.GetFileName(imagepath);
-            }
+            notifier.ShowError("Mahsulot yaratishda xato yuz berdi.");
         }
+    }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+    private void btnClear_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        Clear();
+    }
+
+    private void phone_number_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        TextBox textBox = (TextBox)sender;
+        string text = textBox.Text;
+        string filteredText = Regex.Replace(text, "[^0-9]+", "");
+
+        if (text != filteredText)
         {
-            EnableBlur();
-            await GetAllCategory();
-            await GetAllContrAgent();
+            int caretIndex = textBox.CaretIndex;
+            textBox.Text = filteredText;
+            textBox.CaretIndex = caretIndex > 0 ? caretIndex - 1 : 0;
         }
+    }
+
+    private void btnCreateCategory_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        CategoryCreateWindow categoryCreate = new CategoryCreateWindow();
+        categoryCreate.ShowDialog();
+    }
+
+    private void btnCreateContrAgent_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        ContrAgentCreateWindow contrAgentCreate = new ContrAgentCreateWindow();
+        contrAgentCreate.ShowDialog();
+    }
+
+    public async Task GetAllCategory()
+    {
+        categories = await Task.Run(async () => await categoryService.GetAllAsync());
+        if (categories != null && categories.Any())
+        {
+            comboCategory.ItemsSource = categories.Select(a => a.Name);
+            comboCategory.Items.Refresh();
+        }
+    }
+
+    public async Task GetAllContrAgent()
+    {
+        contrAgents = await Task.Run(async () => await contrAgentService.GetAll());
+        if (contrAgents != null && contrAgents.Any())
+        {
+            comboDelivery.ItemsSource = contrAgents.Select(a => a.FirstName);
+            comboDelivery.Items.Refresh();
+        }
+    }
+
+    public void Clear()
+    {
+        txtBarCode.Text = txtProductName.Text = comboCategory.Text = txtQuantity.Text = txtPrice.Text =
+        txtProductPriceSum.Text = comboDelivery.Text = txtNoteAmount.Text = string.Empty;
+    }
+
+    private void BrClose_MouseUp(object sender, MouseButtonEventArgs e)
+    {
+        this.Close();
+        Clear();
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        openFileDialog.Filter = "JPG Files (*.jpg)|*.jpg|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png";
+        if (openFileDialog.ShowDialog() == true)
+        {
+            imagepath = openFileDialog.FileName;
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(imagepath, UriKind.Absolute);
+            bitmap.EndInit();
+            lbImage.Content = Path.GetFileName(imagepath);
+        }
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        EnableBlur();
+        await GetAllCategory();
+        await GetAllContrAgent();
     }
 }
