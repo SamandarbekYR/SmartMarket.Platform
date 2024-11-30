2d5db72251c1cddf14ea82e27a1d7834a21892ab
Bahriddin Abdusalomov <b.abdusalomov@baik.uz>
2024-11-30 10:09:41 +0500

    add payment type

--- src/SmartMarketDesktop.DTOs/DTOs/Partners/PartnerCreateDto.cs
+++ src/SmartMarketDesktop.DTOs/DTOs/Partners/PartnerCreateDto.cs
@@ -7,4 +7,5 @@ public class PartnerCreateDto
     public string PhoneNumber { get; set; } = string.Empty;
     public double? LastPayment { get; set; }
     public DateTime? LastPaymentDate { get; set; }
+    public string PaymentType { get; set; }
 }
