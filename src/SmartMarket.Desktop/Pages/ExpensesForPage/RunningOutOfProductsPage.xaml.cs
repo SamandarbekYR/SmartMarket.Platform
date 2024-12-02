141dbbf34fe4e496a6e368fc1d45e2420f3add5f
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2024-12-02 23:06:19 +0500

    Updated ExpensePages filters

--- src/SmartMarket.Desktop/Pages/ExpensesForPage/RunningOutOfProductsPage.xaml.cs
+++ src/SmartMarket.Desktop/Pages/ExpensesForPage/RunningOutOfProductsPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using SmartMarket.Desktop.Components.ExpenseForComponents;
+using SmartMarket.Service.DTOs.Products.Product;
 using SmartMarketDeskop.Integrated.Services.Products.Product;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SmartMarket.Desktop.Pages.ExpensesForPage
 {
@@ -17,17 +19,41 @@ namespace SmartMarket.Desktop.Pages.ExpensesForPage
             this._productService = new ProductService();
         }
 
+        private async void FilterLoadReports()
+        {
+            EmptyDataRunningOutOfProduct.Visibility = Visibility.Collapsed;
+            Loader.Visibility = Visibility.Visible;
+            St_Products.Children.Clear();
+
+            var productName = filterTextBox.Text;
+            if (!string.IsNullOrEmpty(productName))
+            {
+                var runningOutOfProducts = await Task.Run(async () => await _productService.GetByProductName(productName));
+                ShowRunningOutOfProducts(runningOutOfProducts);
+            }
+            else
+            {
+                Loader.Visibility = Visibility.Collapsed;
+                EmptyDataRunningOutOfProduct.Visibility = Visibility.Visible;
+            }
+        }
+
         public async void GetAll()
         {
             St_Products.Children.Clear();
 
-            int count = 1;
-
             var products = await Task.Run(async () => await _productService.GetFinishedProducts());
 
+            ShowRunningOutOfProducts(products);
+        }
+
+        private void ShowRunningOutOfProducts(List<FullProductDto> products)
+        {
             Loader.Visibility = Visibility.Collapsed;
 
-            if (products != null)
+            int count = 1;
+
+            if (products.Any())
             {
                 foreach (var product in products)
                 {
@@ -47,6 +73,14 @@ namespace SmartMarket.Desktop.Pages.ExpensesForPage
         public void Page_Loaded(object sender, RoutedEventArgs e)
         {
             GetAll();
+        }
+
+        private void filterTextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                FilterLoadReports();
+            }
         }
     }
 }
