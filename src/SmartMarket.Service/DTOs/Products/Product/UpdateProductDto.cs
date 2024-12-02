fb955067f208120dd3cd6f4fb8742449920d902e
Bahriddin Abdusalomov <b.abdusalomov@baik.uz>
2024-12-02 13:14:31 +0500

    update

---
+++ src/SmartMarket.Service/DTOs/Products/Product/UpdateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartMarket.Service.DTOs.Products.Product
+{
+    public class UpdateProductDto
+    {
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+        public bool IsIncrement { get; set; }
+    }
+}
