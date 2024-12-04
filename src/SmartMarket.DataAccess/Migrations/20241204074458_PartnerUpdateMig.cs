797c06ada7b4194dc78b3176d33fc7c6e3852357
Bahriddin Abdusalomov <b.abdusalomov@baik.uz>
2024-12-04 12:45:37 +0500

    migration

---
+++ src/SmartMarket.DataAccess/Migrations/20241204074458_PartnerUpdateMig.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartMarket.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class PartnerUpdateMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentType",
+                table: "partner",
+                newName: "payment_type");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "total_debt",
+                table: "partner",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_payment",
+                table: "partner",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "payment_type",
+                table: "partner",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "payment_type",
+                table: "partner",
+                newName: "PaymentType");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "total_debt",
+                table: "partner",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "last_payment",
+                table: "partner",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PaymentType",
+                table: "partner",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
