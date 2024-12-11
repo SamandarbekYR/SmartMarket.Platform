106017a6be8aab0d733127f637c230093106ad8a
Bahriddin Abdusalomov <123171397+bahriddin-abdusalomov@users.noreply.github.com>
2024-12-11 23:49:02 +0500

    commit

--- src/SmartMarket.Service/Common/ServiceValidation/PhoneNumberValidators.cs
+++ src/SmartMarket.Service/Common/ServiceValidation/PhoneNumberValidators.cs
@@ -6,7 +6,7 @@ namespace SmartMarket.Service.Common.ServiceValidation
     {
         public static bool IsValid(string phoneNumber)
         {
-            string pattern = @"^\+998(33|77|88|50|90|91|93|94|95|97)\d{7}$";
+            string pattern = @"^\+998(33|77|88|50|90|91|99|93|94|95|97)\d{7}$";
 
             return Regex.IsMatch(phoneNumber, pattern);
         }
